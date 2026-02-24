864b810c5c646347ea6d03fb880af5d67b585851
invitacionesauramx-ctrl <invitacionesauramx@gmail.com>
2026-02-24 10:41:08 -0600

    config.js

--- config.js
+++ config.js
@@ -1,1 +1,46 @@
 
+const CONFIG = {
+
+  // ===== DATOS PRINCIPALES =====
+  nombre: "Liliana",
+  fechaTexto: "29 · 12 · 2026",
+  fechaCuentaRegresiva: "December 29, 2026 17:00:00",
+
+  // ===== PADRES Y PADRINOS =====
+  padres: [
+    "Ana Cristina Huerta Sandoval",
+    "Pablo Sanchez Cornejo"
+  ],
+
+  padrinos: [
+    "Berenice Perez Nuñez",
+    "Carlos Mendoza Salgado"
+  ],
+
+  mensaje: "Hay momentos en la vida que son especiales por sí solos, pero compartirlos con quienes más quiero los convierte en inolvidables. Te invito a celebrar conmigo este día tan especial.",
+
+  // ===== CEREMONIA =====
+  ceremonia: {
+    lugar: "Iglesia Espíritu Santo",
+    hora: "17:00 hrs",
+    direccion: "Leandro Valle #453",
+    ciudad: "Potacio, 60924",
+    mapa: "https://www.google.com/maps/search/Iglesia+Espiritu+Santo"
+  },
+
+  // ===== RECEPCIÓN =====
+  recepcion: {
+    lugar: "Zona Eventos",
+    hora: "20:00 hrs",
+    direccion: "Av. Manuel L. Barragán 134",
+    ciudad: "Anáhuac, 60957",
+    mapa: "https://www.google.com/maps/search/Zona+Eventos+Anahuac+60957"
+  },
+
+  // ===== RSVP =====
+  telefonoWhatsApp: "527531006781",
+
+  // ===== GALERÍA =====
+  hashtag: "#XVLiliana2026"
+
+};
